b0b86d4077c6515645a7df44e45a4691372062b3
Koussay Dhifi <koussaydhifi48@gmail.com>
2021-06-17 11:17:33 +0100

    Added an upload section

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
-from flask import Flask
+from flask import Flask,render_template,request
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = './uploads' #<-- le fichier que nous allons télécharger dans
 
 
 
-@app.route('/')
-def home():
-	return "hello world"
+#la fonction pour uploader un fichier
+@app.route('/upload',methods=["GET","POST"])
+def upload():
+	return render_template('upload.html')
 
+
+
+@app.route("/uploader",methods=['GET','POST'])
+def uploader():
+	if request.method == "POST":
+		f = request.files['file']
+		f.save(secure_filename(f.filename))
+		return 'file uploaded'
 
 
 
